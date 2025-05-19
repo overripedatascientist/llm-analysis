794590f1ce48b3a0f8fcf62dfd241e4f24cc1d7d
datawranglerai <mint.warios@outlook.com>
2025-05-19 13:09:43 +0100

    Set up React app for GitHub Pages

---
+++ currys-analysis/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import CurrysAnalysis from './components/currys-analysis';
+
+function App() {
+  return (
+    <div className="App">
+      <CurrysAnalysis />
+    </div>
+  );
+}
+
+export default App;
